a9d0b73466d369065869a38612cd1202ad412973
tracey-fans <mike-rf@hotmail.com>
2022-03-12 12:17:42 +0000

    A first working version. This only plays replays of Nick Fuentes

--- src/plugin.video.cozytv/resources/lib/run_addon.py
+++ src/plugin.video.cozytv/resources/lib/run_addon.py
@@ -13,6 +13,7 @@ import xbmcplugin
 import xbmcaddon
 
 import requests.api
+import json
 from bs4 import BeautifulSoup
 import re
 
@@ -32,52 +33,24 @@ def get_url(**kwargs):
 
 def list_categories():
     """
-    Create the list of video categories in the Kodi interface. It does it by looking
-    at all the dropdown items in the hamburger on the front page. Currently it
-    only recognises AIU as a content creator
+    Create the list of video categories in the Kodi interface. Currently only one
+    is available, and it is hard-coded.
     """
     
     
-    r = requests.api.get('https://censored.tv')
-    
-    soup = BeautifulSoup(r.content, 'html.parser')
-    
-    set_of_hrefs = set()
+    img_lnk = "https://prd.foxtrotstream.xyz/a/av/fb67015b68ab4b5c842d29175cdd85c4.webp"
+    info_txt = "Nick Fuentes"
+    list_item = xbmcgui.ListItem(label=info_txt)
+    list_item.setArt({'thumb': img_lnk,
+                      'icon': img_lnk,
+                      'fanart': img_lnk})
+    list_item.setInfo('video', {'title': info_txt, 'genre': info_txt})
 
-    for link in soup.find_all("a", class_="dropdown-item"):
-        # Look for links that are relative and have an image attached. Those are
-        # the shows
-        if link.img != None and link.get("href", None) != None:
-            if urlparse(link["href"]).netloc == "":
-
-                # Looks for duplicates
-                if link["href"] in set_of_hrefs:
-                    # Is a duplicate. Do nothing
-                    pass
-                else:
-                    # Not a duplicate
-
-                    set_of_hrefs.add(link["href"])
-
-                    img_lnk = link.img["src"]
-                    info_txt = link.text.strip()
-                    list_item = xbmcgui.ListItem(label=info_txt)
-                    list_item.setArt({'thumb': img_lnk,
-                                      'icon': img_lnk,
-                                      'fanart': img_lnk})
-                    list_item.setInfo('video', {'title': info_txt, 'genre': info_txt})
-
-                    if "UNSTOPPABLE" in info_txt.upper():
-                        # Make sure AIU sorts at the top of the list. This is for
-                        # my benefit since I like this creator. Could possibly
-                        # become an option?
-                        list_item.setProperty("SpecialSort", "top")
-
-                    url = get_url(action='listing', category=link["href"])
-                    # is_folder = True means that this item opens a sub-list of lower level items.
-                    is_folder = True
-                    # Add our item to the Kodi virtual folder listing.
-                    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    url = get_url(action='listing', category="/cache/nick/replays")
+    # is_folder = True means that this item opens a sub-list of lower level items.
+    is_folder = True
+    # Add our item to the Kodi virtual folder listing.
+    xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
 
     # Add a sort method for the virtual folder items (alphabetically, ignore articles)
     xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
@@ -94,63 +67,32 @@ def list_videos(category, page):
     """
     
     if page > 1:
-        page_str = "?page={0}".format(page)
+        page_str = ""
     else:
-        page_str = ""  # ...or ?page=1 also works.
-    r = requests.api.get(urljoin('https://censored.tv', category + page_str))
+        page_str = ""
+    r = requests.api.get(urljoin('https://api.cozy.tv', category + page_str))
     
-    soup = BeautifulSoup(r.content, 'html.parser')
+    j = json.loads(r.text)
+    
+    for item in j['replays']:
+      
+        best_title = item['title']
+        best_href = item['id']
 
-
-    for link in soup.find_all("div", class_="card my-3 my-lg-0"):
-        
-        best_href = None
-        best_title = "No title"
-        best_img = ""
-        bug_img = ""
-        
-        try:
-            bug_img = urljoin("https://censored.tv", link.img["src"])
-        except:
-            pass
-        
-        
-        # Try to parse the "style" component to get a single-quote-denominated url
-        # out of it. Don't worry too much if anything fails, we can always fall back
-        # to an empty picture.
-        if link.img["style"] != None:
-          
-            p = re.compile("background-image:.*url\('(.*)'.*")
-            m = p.match(link.img["style"])
-            
-            if m != None:
-                try:
-                    best_img = m.group(1)
-                except:
-                    pass
-                    
-        
-                
-            for n in link.find_all("div", class_="card-body"):
-                for q in n.find_all("a"):
-                    best_href = q["href"]
-                    for t in q.find_all(class_="card-title"):
-                        best_title = t.text
-                            
         if best_href != None:
 
             # Create a list item with a text label and a thumbnail image.
-            list_item = xbmcgui.ListItem(label=best_title, label2="XYZ")
+            list_item = xbmcgui.ListItem(label=best_title, label2=item['id'])
             # Set additional info for the list item.
-            list_item.setInfo('video', {'title': best_title, 'genre': 'Censored TV'})
+            list_item.setInfo('video', {'title': best_title, 'genre': 'Nick Fuentes'})
             # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
             # Here we use the same image for all items for simplicity's sake.
-            list_item.setArt(
-                {'thumb' : best_img,       # Next to item in list
-                 'icon'  : best_img,       # Not used?
-                 'fanart': bug_img,        # Background.
-                 'poster': best_img        # Main image for vid
-                })
+            #list_item.setArt(
+            #    {'thumb' : best_img,       # Next to item in list
+            #     'icon'  : best_img,       # Not used?
+            #     'fanart': bug_img,        # Background.
+            #     'poster': best_img        # Main image for vid
+            #    })
             # Set 'IsPlayable' property to 'true'.
             # This is mandatory for playable items!
             list_item.setProperty('IsPlayable', 'true')
@@ -170,20 +112,20 @@ def list_videos(category, page):
     
     
     # Add a "Next" option if supported
-    if True:
-      
-        if soup.find_all(class_="page-link", rel="next"):
-            # The page has a "Next" button. This will normally be the case, except
-            # if it's the last page of content
+    #if True:
+    #  
+    #    if soup.find_all(class_="page-link", rel="next"):
+    #        # The page has a "Next" button. This will normally be the case, except
+    #        # if it's the last page of content
 
-            list_item = xbmcgui.ListItem(label=xbmc.getLocalizedString(209))  # "Next"
-            url = get_url(action='listing', category=category, page=str(page+1))
-            # is_folder = True means that this item opens a sub-list of lower level items.
-            is_folder = True
-            # set a special sort property
-            list_item.setProperty("SpecialSort", "bottom")
-            # Add our item to the Kodi virtual folder listing.
-            xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    #        list_item = xbmcgui.ListItem(label=xbmc.getLocalizedString(209))  # "Next"
+    #        url = get_url(action='listing', category=category, page=str(page+1))
+    #        # is_folder = True means that this item opens a sub-list of lower level items.
+    #        is_folder = True
+    #        # set a special sort property
+    #        list_item.setProperty("SpecialSort", "bottom")
+    #        # Add our item to the Kodi virtual folder listing.
+    #        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
         
         
     
@@ -200,137 +142,7 @@ def play_video(path):
     :type path: str
     """
     
-    # We have got to this point without signing in. At this stage can just do a sign
-    # in and read. The session doesn't need to persist beyond this.
-    
-    session = requests.Session()
-    
-    rr = session.get("https://censored.tv/login")
-
-    soup = BeautifulSoup(rr.content, 'html.parser')
-    
-    
-    the_email = xbmcaddon.Addon().getSettingString('email')
-    the_password = xbmcaddon.Addon().getSettingString('password')
-    the_preferred_resolution = "1920x1080"    # Will make this one a setting later
-    
-
-    post = ""
-
-    for m in soup.find_all("form"):
-        is_login = False
-        try:
-            is_login = (m['action'] == '/login')
-        except KeyError:
-            pass
-        if is_login:
-            for q in m.find_all("input"):
-                if q["name"] == "_token":
-                    # Need to have received a token
-                    post = m["action"]
-                    post_val = {q["name"]: q["value"], "email": the_email, "password": the_password}
-
-
-    if len(post) > 1:
-        r = session.post(urljoin("https://censored.tv", post), post_val)
-
-        # Discard r, we don't need it. All we need are the cookies.
-      
-    
-    ## Now load in the requested file
-    #
-    
-    r3 = session.get(urljoin("https://censored.tv", path))
-
-    soup3 = BeautifulSoup(r3.content, 'html.parser')
-
-
-    content_links = []
-
-    for link in soup3.find_all(type="application/x-mpegURL"):
-        content_links.append(link.get('src'))
-        
-
-
-    if len(content_links) < 1:
-        # No content. This tends to happen if the user's password is wrong. Send
-        # a message to that extent. TODO: use the translation system!!
-        dialog = xbmcgui.Dialog()
-        dialog.notification('Password wrong', 'Could not log onto Censored.TV. Check e-mail and password settings in the Configuration of this add-on.', xbmcgui.NOTIFICATION_INFO, 5000)
-        xbmc.log("CENSOREDTV: No content links found. Exiting", xbmc.LOGINFO)
-        return
-
-
-    ## Read the first content file
-    #
-    
-    r4 = session.get(content_links[0])
-    
-
-
-    ## Parse the contents to get the right resolution
-    #
-    f_5 = ""
-    f_6 = ""
-
-    found_line_preferred = False
-    found_line_other = False
-
-    for line in r4.text.splitlines():
-        if line[:18] == "#EXT-X-STREAM-INF:":
-            if line.find("RESOLUTION=" + the_preferred_resolution) >= 0:
-                found_line_preferred = True
-            elif line.find("RESOLUTION=") >= 0:
-                found_line_other = True
-        else:
-            if found_line_preferred:
-                f_5 = line
-                break
-            if found_line_other and f_6 == "":
-                f_6 = line
-            found_line_preferred = False
-            found_line_other = False
-
-
-    if len(f_5) <= 0 and len(f_6) <= 0:
-        xbmc.log("CENSOREDTV: Haven't found the right resolution", xbmc.LOGINFO)
-        return
-
-
-
-    if f_5 != "":
-        f_7 = urljoin(content_links[0], f_5)
-    else:
-        f_7 = urljoin(content_links[0], f_6)
-
-
-
-    ## Read the file
-    #
-    r_5 = session.get(f_7)
-    
-
-    s_7 = set()
-
-
-    ## Look for any non-empty line that doesn't start "#":
-    #
-    for line in r_5.text.splitlines():
-        if len(line) > 0 and line[0] != '#':
-            s_7.add(line)
-
-
-    if len(s_7) == 0:
-        xbmc.log("CENSOREDTV: Did not find content", xbmc.LOGINFO)
-        return
-    elif len(s_7) == 1:
-        # Exactly one piece of content pointed to. Send that piece to Kodi, it works
-        # better to do that way
-        SEND_1 = urljoin(f_7, next(iter(s_7)))
-    else:
-        # More than one piece of content. We have no choice but to send the whole thing
-        SEND_1 = f_7
-
+    SEND_1 = urljoin('https://cozycdn.foxtrotstream.xyz/replays/', 'nick/' + path + '/index.m3u8')
 
     # Create a playable item with a path to play.
     play_item = xbmcgui.ListItem(path=SEND_1)
